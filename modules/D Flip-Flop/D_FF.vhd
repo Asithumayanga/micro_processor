c1d6edff2aa4a5e32f5a1c936bba19d72df936f2
therealoshii <vithanageoshan@gmail.com>
2026-04-29 15:16:58 +0530

    Add D Flip-Flop VHDL implementation
    Implement D Flip-Flop with reset functionality.

---
+++ modules/D Flip-Flop/D_FF.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity D_FF is
+    Port ( D : in STD_LOGIC;
+           Res : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           Q : out STD_LOGIC); -- Q_bar is not instantiated because,
+end D_FF;                      -- it is unused in the register bank
+
+architecture Behavioral of D_FF is
+
+begin
+    process (Clk) begin
+        if (rising_edge(Clk)) then
+            if Res = '1' then         --Clear output when reset is high
+                Q <= '0';
+            else                      --Else output is same as input
+                Q <= D;
+            end if;
+        end if;
+    end process;
+
+end Behavioral;
