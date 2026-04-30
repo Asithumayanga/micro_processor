89afd944399aca39bbfd15fecdb7a0afec7a4f98
therealoshii <vithanageoshan@gmail.com>
2026-04-30 05:58:55 +0530

    Refactor output logic in Decoder_2_to_4

--- modules/Decoder/Decoder_2_to_4.vhd
+++ modules/Decoder/Decoder_2_to_4.vhd
@@ -2,15 +2,17 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Decoder_2_to_4 is
-    Port ( I  : in  std_logic_vector(1 downto 0);
-           EN : in  STD_LOGIC;
-           Y  : out std_logic_vector(3 downto 0));
+    Port ( I : in STD_LOGIC_VECTOR (1 downto 0);
+           EN : in STD_LOGIC;
+           Y : out STD_LOGIC_VECTOR (3 downto 0));
 end Decoder_2_to_4;
 
 architecture Behavioral of Decoder_2_to_4 is
+
 begin
-    Y(0) <= EN and (not I(1)) and (not I(0));
-    Y(1) <= EN and (not I(1)) and      I(0);
-    Y(2) <= EN and      I(1)  and (not I(0));
-    Y(3) <= EN and      I(1)  and      I(0);
+    Y(0) <= (NOT I(0) AND NOT I(1)) AND EN;
+    Y(1) <= (I(0) AND NOT I(1)) AND EN;
+    Y(2) <= (NOT I(0) AND I(1)) AND EN;
+    Y(3) <= (I(0) AND I(1)) AND EN;
+    
 end Behavioral;
