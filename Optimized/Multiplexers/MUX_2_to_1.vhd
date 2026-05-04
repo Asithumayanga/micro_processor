7dc92e3930527fce1caf0a7741601f2fb2195353
therealoshii <vithanageoshan@gmail.com>
2026-05-05 01:21:47 +0530

    2 way 3-bit mux

--- Optimized/Multiplexers/MUX_2_to_1.vhd
+++ Optimized/Multiplexers/MUX_2_to_1.vhd
@@ -2,22 +2,14 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity MUX_2_to_1 is
-    Port (
-        Sel : in  std_logic;
-        A   : in  std_logic_vector(2 downto 0);
-        B   : in  std_logic_vector(2 downto 0);
-        O   : out std_logic_vector(2 downto 0)
-    );
+    Port ( A : in  STD_LOGIC_VECTOR(2 downto 0);
+           B : in  STD_LOGIC_VECTOR(2 downto 0);
+           S : in  STD_LOGIC;
+           Y : out STD_LOGIC_VECTOR(2 downto 0));
 end MUX_2_to_1;
 
 architecture Behavioral of MUX_2_to_1 is
+
 begin
-    process(Sel, A, B)
-    begin
-        if Sel = '0' then
-            O <= A;
-        else
-            O <= B;
-        end if;
-    end process;
-end Behavioral;
+    Y <= A when S = '0' else B;
+end behavioral;
