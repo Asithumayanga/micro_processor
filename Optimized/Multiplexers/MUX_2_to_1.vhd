cb9a9d9324284d620653638527827ab2894448b4
therealoshii <vithanageoshan@gmail.com>
2026-05-06 16:44:51 +0530

    Update MUX_2_to_1.vhd

--- Optimized/Multiplexers/MUX_2_to_1.vhd
+++ Optimized/Multiplexers/MUX_2_to_1.vhd
@@ -1,16 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity MUX_2_to_1 is
-        port(
-            Sel : in std_logic;                                
-            A : in std_logic_vector(2 downto 0);             
-            B : in std_logic_vector(2 downto 0);             
-            O : out std_logic_vector(2 downto 0)             
-        );
-end MUX_2_to_1;
+entity MUX_2_to_1_4bit is
+    Port ( A : in STD_LOGIC_VECTOR(3 downto 0);
+           B : in STD_LOGIC_VECTOR(3 downto 0);
+           Sel : in STD_LOGIC;
+           O : out STD_LOGIC_VECTOR(3 downto 0));
+end MUX_2_to_1_4bit;
 
-architecture Behavioral of MUX_2_to_1 is
+architecture Behavioral of MUX_2_to_1_4bit is
 
 begin
     O <= A when Sel = '0' else B;
